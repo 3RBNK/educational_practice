45ac79eab0bea59e6c2cedbc0234cd0956138ed1
3RBNK <dawidenkokirill@gmail.com>
2024-06-21 12:25:42 +0400

    add test convert to metric system

--- algorithms/task/1_convert_to_metric_system.h
+++ algorithms/task/1_convert_to_metric_system.h
@@ -8,18 +8,36 @@
 #include <stdio.h>
 
 
-void convert_to_metric_system() {
-    int inch;
-    printf("Input inch: \n");
-    scanf("%d", &inch);
-
+void convert_to_metric_system(const double inch) {
     const double inch_in_sm = inch * 2.54;
 
     const int m = (int) inch_in_sm / 100;
     const int sm = (int) inch_in_sm % 100;
     const double mm =  (int) (inch_in_sm * 100) % 100 / 10.0;
 
-    printf("m: %d, sm: %d, mm: %.1lf", m, sm, mm);
+    printf("m: %d, sm: %d, mm: %.1lf\n", m, sm, mm);
+}
+
+
+void test_convert_to_metric_system_only_mm() {
+    convert_to_metric_system(0.3);
+}
+
+
+void test_convert_to_metric_system_only_mm_and_sm() {
+    convert_to_metric_system(1);
+}
+
+
+void test_convert_to_metric_system_all_metric() {
+    convert_to_metric_system(205);
+}
+
+
+void test_convert_to_metric_system() {
+    test_convert_to_metric_system_only_mm();                  // 7.6 mm
+    test_convert_to_metric_system_only_mm_and_sm();      // 2 sm 5.4 mm
+    test_convert_to_metric_system_all_metric();     // 5 m 20 sm 7.0 mm
 }
 
 #endif //PRACTICE_1_CONVERT_TO_METRIC_SYSTEM_H
--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include "algorithms/task/8_find_convex_hull.h"
+#include "algorithms/task/1_convert_to_metric_system.h"
 
 int main(void) {
-    char inputfile[] = "/home/bnkr/CLionProjects/practice/CONVEX.IN";
-    const char outputfile[] = "/home/bnkr/CLionProjects/practice/CONVEX.OUT";
+    // char inputfile[] = "/home/bnkr/CLionProjects/practice/CONVEX.IN";
+    // const char outputfile[] = "/home/bnkr/CLionProjects/practice/CONVEX.OUT";
+    //
+    // find_convex_hull(inputfile, outputfile);
 
-    find_convex_hull(inputfile, outputfile);
+    test_convert_to_metric_system();
 
     return 0;
 }
