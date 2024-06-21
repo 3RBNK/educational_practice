2130a57b8257b9c1b04c4462fc4b03e950d64687
3RBNK <dawidenkokirill@gmail.com>
2024-06-21 12:43:46 +0400

    add test points inside ellipse

--- algorithms/task/3_points_inside_ellipse.h
+++ algorithms/task/3_points_inside_ellipse.h
@@ -5,12 +5,17 @@
 #ifndef PRACTICE_3_POINTS_INSIDE_ELLIPSE_H
 #define PRACTICE_3_POINTS_INSIDE_ELLIPSE_H
 
+#include <stdio.h>
 #include <math.h>
+#include <assert.h>
 
 
 int get_points_inside_ellipse(const double a, const double b) {
     const int whole_a = (int) fabs(a);
     const int whole_b =  (int) fabs(b);
+
+    if (whole_a < 1 || whole_b < 1)
+        return 1;
 
     int result = 0;
     for (int i = -whole_a; i < whole_a + 1; i++)
@@ -19,6 +24,26 @@ int get_points_inside_ellipse(const double a, const double b) {
                 result++;
 
     return result;
+}
+
+
+void test_get_points_inside_ellipse_one_dots() {
+    const int result = get_points_inside_ellipse(0.5, 0.5);
+
+    assert(result == 1);
+}
+
+
+void test_get_points_inside_ellipse_more_dots() {
+    const int result = get_points_inside_ellipse(1.5, 1.5);
+
+    assert(result == 5);
+}
+
+
+void test_get_points_inside_ellipse() {
+    test_get_points_inside_ellipse_one_dots();
+    test_get_points_inside_ellipse_more_dots();
 }
 
 #endif //PRACTICE_3_POINTS_INSIDE_ELLIPSE_H
--- main.c
+++ main.c
@@ -1,13 +1,7 @@
-#include "algorithms/task/8_find_convex_hull.h"
-#include "algorithms/task/2_determine_triangle.h"
+#include"algorithms/task/3_points_inside_ellipse.h"
 
 int main(void) {
-    // char inputfile[] = "/home/bnkr/CLionProjects/practice/CONVEX.IN";
-    // const char outputfile[] = "/home/bnkr/CLionProjects/practice/CONVEX.OUT";
-    //
-    // find_convex_hull(inputfile, outputfile);
-
-   test_determine_triangle();
+    test_get_points_inside_ellipse();
 
     return 0;
 }
